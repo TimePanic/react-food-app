65f5c79182d9257716e0b09d6a0c64717534dde5
Dan Lyndon <daniellyndon21@gmail.com>
2020-05-22 03:05:00 +0100

    init commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header.js';
+import MainSetup from './components/MainSetup.js'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+
+  getData = async(e) => {
+    e.preventDefault();
+    // const query = e.target.elements.query.value;
+    // const apiCall = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${process.env.APP_ID}&app_key=${process.env.APP_KEY}&from=0&to=3&calories=591-722&health=alcohol-free`);
+    // const data = await apiCall.json();
+
+    // console.log(data);
+    console.log(process.env.APP_ID);
+  };
+
+  render(){
+    return (
+      <div id='app'>
+        <Header />
+        <MainSetup
+          getData={this.getData}
+        />
+      </div>
+    )
+  }
 }
 
 export default App;
