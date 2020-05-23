e3ff13b6184ed4351a4f59bef2d8d68d6f1460b0
Dan Lyndon <daniellyndon21@gmail.com>
2020-05-23 03:22:37 +0100

    Preparing for first build

--- src/App.js
+++ src/App.js
@@ -2,19 +2,30 @@ import React, { useState } from 'react';
 import './App.css';
 import Header from './components/Header.js';
 import RecipeCard from './components/RecipeCard.js';
+import Alert from './components/Alert';
 
 
 const App = () => {
 
   const [recipe, setRecipe] = useState([]);
   const [query, setQuery] = useState("");
-  // const [alert, setAlert] = useState("");
+  const [alert, setAlert] = useState("");
 
   const getData = async() => {
-    const apiCall = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${process.env.REACT_APP_ID}&app_key=${process.env.REACT_APP_KEY}&from=0&to=20`);
+    if (query !== "") {
+      const apiCall = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${process.env.REACT_APP_ID}&app_key=${process.env.REACT_APP_KEY}&from=0&to=20`);
     const data = await apiCall.json();
-    setRecipe(data.hits);
-    setQuery("");
+      if (!data.more) {
+        setAlert("No food found with that name");
+      } else {
+        setRecipe(data.hits);
+        setQuery("");
+        setAlert("");
+      }
+    } else {
+      setAlert("Please fill in the form");
+    }
+    
     
   };
 
@@ -36,6 +47,7 @@ const App = () => {
             <input type="text" placeholder="Search for food here" autoComplete="off" onChange={onChangeQuery} value={query}/>
             <button type='submit'>&rarr;</button>
           </form>
+          {alert!== "" && <Alert alert={alert}/>}
         </div>
         <div className='dashboard'>
           { recipe !== [] && recipe.map(recipe => (
